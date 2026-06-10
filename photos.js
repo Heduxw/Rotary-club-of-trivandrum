c72c437c6589e321de6bd02894f4c198337f6b58
Heduxw <adwaitssingh@gmail.com>
2026-06-10 20:50:59 +0530

    Adding images for the slideshow

--- photos.js
+++ photos.js
@@ -2,4 +2,5 @@ const slideshowPhotos = [
   { src: "Resources/slides/photo 1.jpg", caption: "Family Meeting" },
   { src: "Resources/slides/photo 3.jpeg", caption: "Service Project" },
   { src: "Resources/slides/photo 4.jpeg", caption: "Installation Ceremony" },
+    { src: "Resources/slides/photo 5.jpeg", caption: "Installation Ceremony" },
 ];
