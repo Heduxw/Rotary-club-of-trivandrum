21c32c6eef75c534826dd693d178647e9e44feb6
Heduxw <adwaitssingh@gmail.com>
2026-06-13 13:23:45 +0530

    Update photos.js

--- scripts/photos.js
+++ scripts/photos.js
@@ -1,6 +1,6 @@
 const slideshowPhotos = [
-  { src: "../media/slides/photo 1.jpg", caption: "Family Meeting" },
+  { src: "media/slides/photo 1.jpg", caption: "Family Meeting" },
   { src: "media/slides/photo 3.jpeg", caption: "Service Project" },
   { src: "media/slides/photo 4.jpeg", caption: "Installation Ceremony" },
-  { src: "../media/slides/photo 5.jpeg", caption: "Installation Ceremony" },
+  { src: "media/slides/photo 5.jpeg", caption: "Installation Ceremony" },
 ];
